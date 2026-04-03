68cb41a482fd3da34b6b809dbbfac1a358dc1e15
dxol03 <151935037+dxol03@users.noreply.github.com>
2026-04-03 09:56:12 +0900

    Update 4-1.c

---
+++ code/4-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdio.h>
+
+void echo() {
+    char buf[5];
+    scanf("%5s",buf);
+    printf("%s\n",buf);
+}
+
+int main() {
+    int x = 42;
+    echo();
+    return 0;
+}
