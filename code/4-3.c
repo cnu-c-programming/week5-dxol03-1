c27b263971ba5c520cb4046ab983a727b6debd32
dxol03 <151935037+dxol03@users.noreply.github.com>
2026-04-03 13:28:11 +0900

    Update 4-3.c

---
+++ code/4-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10,20,30,40,50};
+    int size = 5;
+    int index;
+
+    scanf("%d", &index);
+
+    if (index < 0 || index >= size) {
+        printf("Error!\n");
+    }
+    else {
+        printf("%d\n",arr[index]);
+    }
+    
+
+
+    return 0;
+}
